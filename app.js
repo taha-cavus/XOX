332fd0d58748111f4e51fc8e2df866fb6a47fb85
Taha Faruk Cavus <tahacavus@outlook.com>
2022-05-11 23:01:48 +0300

    js basic complete

---
+++ app.js
@@ -0,0 +1,18 @@
+var main = document.querySelector(".main")
+var vsDOM = document.querySelector("#vs")
+var botDOM = document.querySelector("#bot")
+
+vsDOM.addEventListener("click",()=>{
+    doVS()
+})
+botDOM.addEventListener("click",()=>{
+    doBot()
+})
+
+
+function doVS(){
+
+}
+function doBot(){
+
+}
