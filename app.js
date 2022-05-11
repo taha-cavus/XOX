90f3b07b416adb3259106ef76c81a559664b0926
Taha Faruk Cavus <tahacavus@outlook.com>
2022-05-12 01:27:51 +0300

    some set. style

--- app.js
+++ app.js
@@ -1,48 +1,57 @@
-var main = document.querySelector(".main")
-var vsDOM = document.querySelector("#vs")
-var botDOM = document.querySelector("#bot")
+var border = document.querySelector(".border")
+createMenuPage();
 
-vsDOM.addEventListener("click", () => {
-    doVS()
-})
-botDOM.addEventListener("click", () => {
-    doBot()
-})
+function createMenuPage() {
+    var main = document.createElement("div")
+    main.classList.add("main")
+
+    var vsbtn = document.createElement("button")
+    vsbtn.classList.add("btns")
+    vsbtn.textContent = "1 VS 1"
+    var botbtn = document.createElement("button")
+    botbtn.classList.add("btns")
+    botbtn.textContent = "Single Player"
+
+    border.appendChild(main)
+    main.appendChild(vsbtn)
+    main.appendChild(botbtn)
+    vsbtn.addEventListener("click", () => {  
+        play()
+    })
+    botbtn.addEventListener("click", () => {
+        play()
+    })
 
 
-function doVS() {
-    clearMainSection();
-
-    // for (let y = 0; y < xoxList.length; y++) {
-    //     for (let x = 0; x < xoxList.length; x++) {
-    //         var create = document.createElement("button")
-    //         create.textContent = xoxList[y][x]
-    //         main.appendChild(create)
-    //     }
-    // }
-}
-function doBot() {
-
-}
-
-function clearMainSection() {
-    main.remove()
-    function createMainSection() {
-        var main = document.createElement("button")
-        main.classList.add("main")
-        document.querySelector(".border").appendChild(main);
+    //clear main section
+    function deleteMainSection() {
+        main.remove();
     }
-    createMainSection();
+    function createMainSection() {
+        var main = document.createElement("div")
+        main.classList.add("main")
+        main.classList.add("playGround")
+        border.appendChild(main)
+        
+    }
+    function play() {
+        debugger
+        deleteMainSection()
+        createMainSection()
+        for (let y = 0; y < list.length; y++) {
+            for (let x = 0; x < list.length; x++) {
+                var create = document.createElement("button")
+                create.textContent = list[y][x]
+                create.classList.add("xoBtns")
+                document.querySelector(".main").appendChild(create)
+            }
+        }
+    }
 }
 
 
 
-//XOX LİST
-var xoxList = [["", "", ""], ["", "", ""], ["", "", ""]]
-console.log(``, xoxList)
-
-
-
+var list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
 
 
